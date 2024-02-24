6db7c3ecae43041172bf65d451b24c56376323c5
gan1khinm <ganikhinm@mail.ru>
2024-02-24 12:03:32 +0300

    Initial commit

---
+++ js/activeState.js
@@ -0,0 +1,17 @@
+var currentPage = window.location.href;
+var pages = document.querySelectorAll('a');
+
+pages.forEach(function(page) {
+    var dataId = page.getAttribute('data-id');
+    if (currentPage.includes(dataId)) {
+        page.classList.add('active');
+    }
+})
+
+// if (currentPage.includes("chat")) {
+//     document.querySelector('a[href="../html/chat.html"]').classList.add('active')
+// } else if (currentPage.includes("contacts")) {
+//     document.querySelector('a[href="../html/contacts.html"]').classList.add('active')
+// } else if (currentPage.includes("initial")) {
+//     document.querySelector('a[href="../html/initial.html"]').classList.add('active')
+// }
