f75735184af6884ee364c629a9e8b9db3355a64c
gan1khinm <ganikhinm@mail.ru>
2024-02-25 19:58:25 +0300

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+//
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   const port = process.env.PORT || 3000; // Порт по умолчанию 3000, если не указан в переменной окружения
+//   await app.listen(port);
+// }
+//
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+      AppModule,
+  );
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  // app.useStaticAssets(join(__dirname, '..', 'public/js'));
+  // app.useStaticAssets(join(__dirname, '..', 'public/css'));
+  // app.useStaticAssets(join(__dirname, '..', 'public/img'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
   const port = process.env.PORT || 3000; // Порт по умолчанию 3000, если не указан в переменной окружения
   await app.listen(port);
 }
